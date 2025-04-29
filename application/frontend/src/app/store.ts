0a9a02dcff57e0cf402505d31c2c6c36ce9fe7f7
Andrea D. Gardini <diazgand@fit.cvut.cz>
2025-04-29 08:31:41 +0200

    SW-21 Fixing store

--- application/frontend/src/app/store.ts
+++ application/frontend/src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, Action } from '@reduxjs/toolkit'
-import { thunk } from 'redux-thunk';
 
 import rootReducer, { RootState } from '../reducers/root.reducer'
 
 const store = configureStore({
 	reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 })
 
 export default store;
