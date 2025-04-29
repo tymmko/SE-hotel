d8fa31670527594846fb0dad6461a6e654bf23fe
LuisGomezdeTerreros <gomezyoramas@gmail.com>
2025-04-29 04:25:15 +0200

    pricehistory

--- application/backend/src/routes/apiRoutes.js
+++ application/backend/src/routes/apiRoutes.js
@@ -6,7 +6,7 @@ const reservationController = require('../controllers/reservation.controller');
 const guestController = require('../controllers/guest.controller');
 const roomController = require('../controllers/room.controller');
 const billController = require('../controllers/bill.controller');
-
+const priceHistoryController = require('../controllers/priceHistory.controller');
 
 
 router.get('/reservations', reservationController.getReservations);
@@ -23,6 +23,16 @@ router.delete('/rooms/:id', roomController.deleteRoom);
 router.get('/bills', billController.getAllBills);
 router.get('/bills/:id', billController.getBill);
 router.post('/bills', billController.createBill);
+router.put('/bills/:id', billController.updateBill);
+
+// Price History routes
+router.get('/pricehistory', priceHistoryController.getAllPriceHistories);
+router.get('/pricehistory/:id', priceHistoryController.getPriceHistory);
+router.get('/pricehistory/room/:roomId', priceHistoryController.getPriceHistoryByRoomId);
+router.post('/pricehistory', priceHistoryController.createPriceHistory);
+router.put('/pricehistory/:id', priceHistoryController.updatePriceHistory);
+router.delete('/pricehistory/:id', priceHistoryController.deletePriceHistory);
+
 
 
 module.exports = router;
