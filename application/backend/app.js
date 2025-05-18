ef306a1b97adc7823042d4957e90b7ee1bf4f885
Andrea D. Gardini <diazgand@fit.cvut.cz>
2025-05-18 14:28:20 +0200

    Refractoring

--- application/backend/app.js
+++ application/backend/app.js
@@ -4,25 +4,20 @@ const apiRoutes = require('./src/routes/apiRoutes');
 const errorHandler = require('./src/middlewares/errorHandler');
 
 const allowedOrigins = [
-  'http://localhost:3000',
-  'https://hotel-frontend.up.railway.app'
+	'http://localhost:8080',
+	'https://hotel-frontend.up.railway.app'
 ];
 
 const app = express();
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true,
-}));
-
-app.use(cors({
-  origin: 'https://hotel-frontend.up.railway.app',
-  credentials: true,
+	origin: (origin, callback) => {
+		if (!origin || allowedOrigins.includes(origin)) {
+			return callback(null, true);
+		}
+		return callback(new Error('Not allowed by CORS'));
+	},
+	credentials: true,
 }));
 
 app.use(express.json());
